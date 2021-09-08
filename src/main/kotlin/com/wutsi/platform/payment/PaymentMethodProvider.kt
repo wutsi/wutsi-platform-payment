308c2b170d1541ed78e1f2d54861e337c0236638
htchepannou <htchepannou@expedia.com>
2021-09-08 19:25:39 -0400

    add orange as mobile payment provider

--- src/main/kotlin/com/wutsi/platform/payment/PaymentMethodProvider.kt
+++ src/main/kotlin/com/wutsi/platform/payment/PaymentMethodProvider.kt
@@ -2,5 +2,6 @@ package com.wutsi.platform.payment
 
 enum class PaymentMethodProvider(val shortName: String) {
     PAYMENT_METHOD_PROVDER_INVALID(""),
-    PAYMENT_METHOD_PROVIDER_MTN("mtn")
+    PAYMENT_METHOD_PROVIDER_MTN("mtn"),
+    PAYMENT_METHOD_PROVIDER_ORANGE("orange")
 }
