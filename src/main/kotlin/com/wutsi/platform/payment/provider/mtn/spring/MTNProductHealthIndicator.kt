be7710767f5af9d0afe6359a022b1883e5f88d91
htchepannou <htchepannou@expedia.com>
2021-09-10 08:42:27 -0400

    log failure

--- src/main/kotlin/com/wutsi/platform/payment/provider/mtn/spring/MTNProductHealthIndicator.kt
+++ src/main/kotlin/com/wutsi/platform/payment/provider/mtn/spring/MTNProductHealthIndicator.kt
@@ -1,12 +1,17 @@
 package com.wutsi.platform.payment.provider.mtn.spring
 
 import com.wutsi.platform.payment.provider.mtn.product.Product
+import org.slf4j.LoggerFactory
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
 
 class MTNProductHealthIndicator(
     private val collection: Product
 ) : HealthIndicator {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(MTNProductHealthIndicator::class.java)
+    }
+
     override fun health(): Health {
         try {
             val now = System.currentTimeMillis()
@@ -15,6 +20,7 @@ class MTNProductHealthIndicator(
                 .withDetail("durationMillis", System.currentTimeMillis() - now)
                 .build()
         } catch (ex: Exception) {
+            LOGGER.error("Health failure", ex)
             return Health.down()
                 .withException(ex)
                 .build()
