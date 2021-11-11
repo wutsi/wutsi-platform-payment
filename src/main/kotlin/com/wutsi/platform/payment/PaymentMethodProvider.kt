13eac1b5694ac9640cdeb672aeb2c396a4883078
htchepannou <htchepannou@expedia.com>
2021-11-11 13:01:03 -0500

    update update payment method structure

--- src/main/kotlin/com/wutsi/platform/payment/PaymentMethodProvider.kt
+++ src/main/kotlin/com/wutsi/platform/payment/PaymentMethodProvider.kt
@@ -1,13 +1,8 @@
 package com.wutsi.platform.payment
 
-import com.wutsi.platform.payment.PaymentMethodType.MOBILE
-
-enum class PaymentMethodProvider(
-    val displayName: String,
-    val paymentType: PaymentMethodType
-) {
-    UNKNOWN("", PaymentMethodType.UNKNOWN),
-    MTN("MTN", MOBILE),
-    ORANGE("Orange", MOBILE),
-    NEXTTEL("Nexttel", MOBILE)
+enum class PaymentMethodProvider {
+    UNKNOWN,
+    MTN,
+    ORANGE,
+    NEXTTEL
 }
